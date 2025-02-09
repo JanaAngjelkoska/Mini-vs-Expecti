6d630cf161ca7893cf4fe2be47258ff4788f4694
David <duko_7@outlook.com>
2025-02-09 17:18:41 +0100

    fix: Fix the TT table logic.

--- search/expectimax.py
+++ search/expectimax.py
@@ -2,27 +2,26 @@ from chess import Board, Move
 from typing import Tuple, Optional
 import numpy as np
 from eval.heuristics import EvaluationEngine
-from utility.expectimax_node import ExpectimaxNode
 
 
 class Expectiminimax:
     def __init__(self, eval_engine: EvaluationEngine):
         self.eval_engine = eval_engine
 
-    def execute(self, node: ExpectimaxNode, depth: int, is_max_player: bool) -> Tuple[float, Optional[Move]]:
+    def execute(self, board, depth: int, is_max_player: bool) -> Tuple[float, Optional[Move]]:
 
         if depth == 0:
-            return self.eval_engine.evaluate_position(node.board), None
+            return self.eval_engine.evaluate_position(board), None
 
-        legal_moves = list(node.board.legal_moves)
+        legal_moves = list(board.legal_moves)
 
         if is_max_player:
             best_value = -np.inf
             best_move = None
             for move in legal_moves:
-                node.board.push(move)
-                evaluation, _ = self.execute(node, depth - 1, False)
-                node.board.pop()
+                board.push(move)
+                evaluation, _ = self.execute(board, depth - 1, False)
+                board.pop()
 
                 if evaluation > best_value:
                     best_value = evaluation
@@ -34,9 +33,9 @@ class Expectiminimax:
             best_value = np.inf
             best_move = None
             for move in legal_moves:
-                node.board.push(move)
-                evaluation, _ = self.execute(node, depth - 1, True)
-                node.board.pop()
+                board.push(move)
+                evaluation, _ = self.execute(board, depth - 1, True)
+                board.pop()
 
                 if evaluation < best_value:
                     best_value = evaluation
@@ -49,9 +48,9 @@ class Expectiminimax:
             probabilities = [1.0 / len(legal_moves) for _ in legal_moves]
 
             for move, prob in zip(legal_moves, probabilities):
-                node.board.push(move)
-                evaluation, _ = self.execute(node, depth - 1, True)
-                node.board.pop()
+                board.push(move)
+                evaluation, _ = self.execute(board, depth - 1, True)
+                board.pop()
                 expected_value += prob * evaluation
 
             return expected_value, None
@@ -64,6 +63,3 @@ class Expectiminimax:
 # depth = 3
 # best_value, best_move = expectiminimax.execute(root_node, depth, is_max_player=True)
 # print(f"Best move: {best_move}, Evaluation value: {best_value}")
-message.txt
-3
-KB
