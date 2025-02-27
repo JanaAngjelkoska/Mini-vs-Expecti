35d576baabfa56bacc40435d34685e2e618094f4
JanaAngjelkoska <jana.angjelkoska@hotmail.com>
2025-02-27 15:03:42 +0100

    Heuristics adjusted

--- game.py
+++ game.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 import numpy as np
 
@@ -40,114 +39,113 @@ def print_board_sf_style(b: Board):
     print("    a b c d e f g h")
 
 
-def save_game(g: GameNode, result: str) -> None:
+def save_game(g: GameNode) -> None:
     only_string = str(g.game()).split(']')[-1].strip()
+    result = only_string.split(' ')[-1]
+
+    if result == '*':
+        raise ValueError(f"Cannot save game with PGN: {only_string}\nGame is yet to finish")  # game in progress
+    elif result == '1-0':
+        result = '1'  # white victory
+    elif result == '0-1':
+        result = '0'  # black victory
+    else:
+        result = '2'  # draw
 
     to_save = {
-        'pgn': [only_string],
-        'result': [result]
+        'pgn': only_string,
+        'result': result
     }
 
     if len(os.listdir('./games')) == 0:
         df = pd.DataFrame(to_save)
     else:
         df = pd.read_csv('./games/played.csv')
-        if ((df['pgn'] == only_string) & (df['result'] == result)).any():
-            print(f"Variation:\n{only_string}\nalready exists in the database.", file=sys.stderr)
-            return
-        df_concat = pd.DataFrame(to_save)
-        print(df_concat)
-        df = pd.concat([df, df_concat], axis=0, ignore_index=True)
+        df.append(to_save)
 
-    print('new df:')
-    print(df)
-
-    df.to_csv('./games/played.csv', index=False)
+    df.to_csv('./games/played.csv')
 
 
 if __name__ == '__main__':
 
-    evaluator = Evaluator()
-
-    mini = Minimax(evaluator)
-    expecti = Expectiminimax(evaluator)
-
-    board = Board('4k2r/p1p2p1p/4b2P/8/6p1/3n2P1/r2n4/1RK4R w k - 6 31')
-
-    answer = set(input("Who's " + Fore.RED + "Minimax" + Fore.RESET + "?").lower())
-
-    mini_white = True
-
-    if len(answer.intersection(set("black"))) == 5:
-        print(
-            "Game started with " + Fore.RED + "Minimax" + Fore.RESET + " as black, " + Fore.BLUE + "Expectiminimax" + Fore.RESET + " as white")
-        mini_white = False
-    elif len(answer.intersection(set("white"))) == 5:
-        print(
-            "Game started with " + Fore.RED + "Minimax" + Fore.RESET + " as white, " + Fore.BLUE + "Expectiminimax" + Fore.RESET + " as black")
-    else:
-        raise ValueError(f'Invalid input:\n{''.join(answer)}\nPlease enter either white or black')
-
-    print_board_sf_style(board)
-
     game = pgn.Game()
-    game.setup(board)
 
-    result = None
+    automated = input("Would you like to automate the first move in all games?").lower()
+    first_moves = ['b1c3', 'g1f3', 'c2c4', 'e2e4', 'd2d4']
+    has_played_first = False
 
-    while not board.is_game_over():
-        print(
-            Fore.WHITE + "White (" + f'{Fore.RED + 'Mini' if mini_white else Fore.BLUE + 'expecti'}' + Fore.WHITE + ")" + Fore.RESET + "'s Turn")
-        white_move = None
-        if mini_white:
-            mini_eval, mini_move = mini.search(0, 4, WHITE, -np.inf, np.inf, board)
-            white_move = mini_move
+    for first_move in first_moves:
+
+        evaluator = Evaluator()
+
+        mini = Minimax(evaluator)
+        expecti = Expectiminimax(evaluator)
+
+        board = Board()
+
+        answer = set(input("Who's " + Fore.RED + "Minimax" + Fore.RESET + "?").lower())
+
+        mini_white = True
+
+        if len(answer.intersection(set("black"))) == 5:
+            print(
+                "Game started with " + Fore.RED + "Minimax" + Fore.RESET + " as black, " + Fore.BLUE + "Expectiminimax" + Fore.RESET + " as white")
+            mini_white = False
+        elif len(answer.intersection(set("white"))) == 5:
+            print(
+                "Game started with " + Fore.RED + "Minimax" + Fore.RESET + " as white, " + Fore.BLUE + "Expectiminimax" + Fore.RESET + " as black")
         else:
-            expecti_eval, expecti_move = expecti.search(0, 3, WHITE, board)
-            white_move = expecti_move
-            print("Expecti: " + white_move.__str__() + expecti_eval.__str__())
-
-        print("White moved: ", white_move)
-        game = game.add_variation(white_move)
-
-        board.push(white_move)
-
-        if board.is_game_over():
-            if board.is_checkmate():
-                result = '1-0'
-            else:
-                result = '1/2-1/2'
+            raise ValueError(f'Invalid input:\n{''.join(answer)}\nPlease enter either white or black')
 
         print_board_sf_style(board)
 
-        if board.is_game_over():
-            break
+        while not board.is_game_over():
+            print(
+                Fore.WHITE + "White (" + f'{Fore.RED + 'Mini' if mini_white else Fore.BLUE + 'expecti'}' + Fore.WHITE + ")" + Fore.RESET + "'s Turn")
+            white_move = None
 
-        print(
-            Fore.BLACK + "Black (" + f'{Fore.RED + 'mini' if not mini_white else Fore.BLUE + 'Expecti'}' + Fore.BLACK + ")" + Fore.RESET + "'s Turn")
-
-        black_move = None
-        if mini_white:
-            expecti_eval, expecti_move = expecti.search(0, 3, BLACK, board)
-            print(black_move)
-            black_move = expecti_move
-        else:
-            mini_eval, mini_move = mini.search(0, 4, BLACK, -np.inf, np.inf, board)
-            black_move = mini_move
-            print('at mini' + black_move.__str__() + mini_eval.__str__())
-
-        print("Black moved: ", black_move)
-        game = game.add_variation(black_move)
-
-        board.push(black_move)
-
-        if board.is_game_over():
-            if board.is_checkmate():
-                result = '0-1'
+            if automated and not has_played_first:
+                print("Automated move: ", first_move)
+                white_move = first_move
+                board.push_san(first_move)
+                has_played_first = True
             else:
-                result = '1/2-1/2'
+                if mini_white:
+                    mini_eval, mini_move = mini.search(0, 4, mini_white, -np.inf, np.inf, board)
+                    white_move = mini_move
+                else:
+                    expecti_eval, expecti_move = expecti.search(0, 3, not mini_white, board)
+                    white_move = expecti_move
 
-        print_board_sf_style(board)
-        print(Evaluator.piece_presence)
+                board.push(white_move)
+                game = game.add_variation(white_move)
 
-    save_game(game, result)
+            print("White moved: ", white_move)
+
+            print_board_sf_style(board)
+
+            if board.is_game_over():
+                has_played_first = not has_played_first
+                break
+
+            print(
+                Fore.BLACK + "Black (" + f'{Fore.RED + 'mini' if not mini_white else Fore.BLUE + 'Expecti'}' + Fore.BLACK + ")" + Fore.RESET + "'s Turn")
+
+            black_move = None
+            if mini_white:
+                expecti_eval, expecti_move = expecti.search(0, 3, not mini_white, board)
+                black_move = expecti_move
+            else:
+                mini_eval, mini_move = mini.search(0, 4, mini_white, -np.inf, np.inf, board)
+                black_move = mini_move
+
+            board.push(black_move)
+            print("Black moved: ", black_move)
+
+            game = game.add_variation(black_move)
+
+            print_board_sf_style(board)
+            print(Evaluator.piece_presence)
+
+        save_game(game)
+        has_played_first = False
